26cd922e446331e6f7e2abe63d04cda83ee0e344
mehdi_kidai <mehdikidai@gmail.com>
2024-07-17 14:53:38 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+export const app = express();
+
+
+
+export const server = http.createServer(app);
+export  const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:8000","http://127.0.0.1:8000"],
+        methods: ["GET", "POST"]
+    },
+});
