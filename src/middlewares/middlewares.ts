72679439d6cbd32992a705634927a5aa5bb63215
mehdikidai <mehdikidai@gmail.com>
2024-11-20 17:05:08 +0100

    test

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -8,11 +8,9 @@ const middlewareUrlencoded = bodyParser.urlencoded({ extended: false });
 //-------- auth fn ------
 
 function Auth(req: Request, res: Response, next: NextFunction) {
-
 	const socketKey = req.headers['socket-key'];
 
-	return socketKey !== SOCKET_KEY ? res.status(400).json({ message: 'error' }) : next()
-
+	return socketKey !== SOCKET_KEY ? res.status(400).json({ message: 'error' }) : next();
 }
 
 //------- end auth fn ------
